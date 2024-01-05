3040e969ca1fbfb21a1bf574ca5584d6b0cdf2ce
aesthaltics <aesthaltics@pm.me>
2024-01-05 22:45:19 +0100

    ui update

--- src/components/ui/pattern.tsx
+++ src/components/ui/pattern.tsx
@@ -1,4 +1,7 @@
-import { useState } from "react";
+"use client";
+import { useState, useRef, RefObject } from "react";
+import SlidingWindow, { WindowElement } from "./sliding-window";
+import { Button } from "./button";
 
 type pattern_props = {
 	showPattern: boolean;
@@ -6,6 +9,43 @@ type pattern_props = {
 };
 
 const Pattern = ({ showPattern, simplestPattern }: pattern_props) => {
+	const [currentIndex, setCurrentIndex] = useState(0);
+	const [autoId, setAutoId] = useState<number | undefined>(undefined);
+	const activeCellRef = useRef<HTMLDivElement>(null);
+	const window_ref = useRef<HTMLDivElement>(null);
+
+	const pattern_length = simplestPattern.reduce((acc, cur) => acc + cur, 0);
+
+	const cellWidth = Math.max(
+		35,
+		(window_ref.current?.clientWidth ?? 0) / pattern_length
+	);
+
+	const toggleAuto = () => {
+		if (autoId) {
+			window.clearInterval(autoId);
+			setAutoId(undefined);
+			return;
+		}
+
+		let newAutoId = window.setInterval(() => {
+			setCurrentIndex(
+				(currentIndex) => (currentIndex + 1) % pattern_length
+			);
+		}, 1000);
+		setAutoId(newAutoId);
+		return;
+	};
+
+	const scollToView = (ref: Element | null, index: number) => {
+		if (ref === null) {
+			return;
+		}
+		ref.scrollIntoView({
+			behavior: index === 0 ? "instant" : "smooth",
+			inline: index === 0 ? "start" : "center",
+		});
+	};
 	const patternToSquares = (pattern: number[]) => {
 		let squares: { [index: string]: boolean } = {};
 		let current_index = 0;
@@ -19,26 +59,64 @@ const Pattern = ({ showPattern, simplestPattern }: pattern_props) => {
 				current_index++;
 			}
 		}
-		console.log(squares);
 		return squares;
 	};
 
 	return (
-		<div className="flex flex-col w-full items-center px-2">
-			<p className="text-3xl">{`Det enkleste mønsteret er ${simplestPattern}`}</p>
-			<div className="flex flex-row gap-2 w-full ">
+		<div
+			className="flex flex-col w-full items-center px-4 gap-4"
+			ref={window_ref}
+		>
+			{/* <div className="flex flex-row gap-2 w-full "> */}
+			{simplestPattern.length > 0 && (
+				<div className="flex flex-col text-3xl w-full items-center justify-center">
+					<p>Det enkleste mønsteret er </p>
+					<div className="flex flex-wrap">
+						[
+						{simplestPattern.map((num, index) => (
+							<p key={`pattern text ${index}`}>{num}</p>
+						))}
+						]
+					</div>
+				</div>
+			)}
+			<SlidingWindow>
 				{Object.entries(patternToSquares(simplestPattern)).map(
 					([key, val], i, array) => (
-						<Pattern_square
-							num={key}
-							isActive={val}
-							showPattern={showPattern}
-							key={i}
-							totalAmount={array.length}
-						/>
+						<WindowElement
+							current_index={currentIndex}
+							index={i}
+							key={`window element${i}`}
+							ref={activeCellRef}
+							marker_width={cellWidth / 3}
+							scroll={scollToView}
+						>
+							<Pattern_square
+								num={(1 + +key).toString()}
+								isActive={val}
+								showPattern={showPattern}
+								totalAmount={array.length}
+								cellWidth={cellWidth}
+							/>
+						</WindowElement>
 					)
 				)}
+			</SlidingWindow>
+			<div className="flex gap-5">
+				<Button
+					onClick={() =>
+						setCurrentIndex(
+							(currentIndex) =>
+								(currentIndex + 1) % pattern_length
+						)
+					}
+				>
+					Neste
+				</Button>
+				<Button onClick={toggleAuto}>{autoId ? "Stop" : "Auto"}</Button>
 			</div>
+
+			{/* </div> */}
 		</div>
 	);
 };
@@ -49,6 +127,7 @@ type pattern_square_props = {
 	isActive: boolean;
 	showPattern: boolean;
 	totalAmount: number;
+	cellWidth: number;
 };
 
 const Pattern_square = ({
@@ -56,21 +135,18 @@ const Pattern_square = ({
 	isActive,
 	showPattern,
 	totalAmount,
+	cellWidth,
 }: pattern_square_props) => {
-	const cellWidthPercentage = 100 / totalAmount;
-	const height =
-		Math.floor((window.innerWidth * cellWidthPercentage) / 100) * 0.7;
-	console.log(`height ${height}`);
 	return (
 		<div
-			style={{'width': `${cellWidthPercentage}%` }}
+			style={{ width: `${cellWidth}px` }}
 			className={`${
 				showPattern && isActive ? "bg-blue-600" : "bg-gray-200"
 			} aspect-square rounded opacity-100`}
 		>
 			{!showPattern && (
 				<div
-					style={{ fontSize: `${height}px` }}
+					style={{ fontSize: `${cellWidth * 0.6}px` }}
 					className="flex bg-gradient-to-br from-[#002446] to-[#08ece5] h-full w-full items-center justify-center bg-clip-text"
 				>
 					<p
---
+++ src/components/ui/sliding-window.tsx
@@ -0,0 +1,50 @@
+import React, { forwardRef, ReactNode, useRef } from "react";
+type SlidingWindowProps = {
+	children: ReactNode;
+};
+const SlidingWindow = ({ children }: SlidingWindowProps) => {
+	return (
+		<div className="flex w-full max-w-full justify-center">
+			<div className="flex overflow-scroll gap-1 hide-scrollbar">
+				{children}
+			</div>
+		</div>
+	);
+};
+
+type WindowElementProps = {
+	children: ReactNode;
+	index: number;
+	current_index: number;
+	marker_width: number;
+	scroll: (e:Element|null, index:number) => void
+};
+export const WindowElement = React.forwardRef(function WindowElement(
+	props: WindowElementProps,
+	ref: React.ForwardedRef<HTMLDivElement>
+) {
+	const { children, index, current_index, marker_width, scroll } = props;
+
+	const internalRef = useRef<HTMLDivElement>(null)
+
+	if (index === current_index) {
+		scroll(internalRef.current, index)
+		ref = internalRef
+	}
+	return (
+		<div
+			className="flex flex-col items-center justify-center gap-2"
+			ref={internalRef}
+		>
+			<div
+				style={{ width: `${Math.max(marker_width, 10)}px` }}
+				className={`aspect-square rounded-full ${
+					index === current_index ? "bg-blue-500" : "bg-gray-400"
+				}`}
+			></div>
+			{children}
+		</div>
+	);
+});
+
+export default SlidingWindow;
